b2df7c0e5f078f93fd3d507c2143ff4e54c19ec5
Rickson2019 <JudaoZhong@gmail.com>
2022-07-02 22:02:25 -0700

    Added barebone

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import './App.css';
 
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from 'react-dnd-touch-backend'
+
+
 import { BoardContextProvider } from './contexts/BoardContext'
 import Board from './components/Board'
+
 
 function App() {
   return (
 
     <div className="App">
-      <BoardContextProvider>
-        <Board />
-      </BoardContextProvider>
+      <DndProvider backend={HTML5Backend}>
+        {/* <DndProvider backend={TouchBackend}> */}
+          <BoardContextProvider>
+            <Board />
+          </BoardContextProvider>
+        {/* </DndProvider> */}
+      </DndProvider>
     </div>
 
   );
