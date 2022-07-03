99e3e9e8e0e32bdf68629511149e17cf42c115a2
Rickson2019 <JudaoZhong@gmail.com>
2022-07-02 22:02:46 -0700

    Created table with styled componentes

---
+++ src/components/WelcomePage/table.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+
+
+// Styled Components
+const seat = styled.div`
+    width : 90px;
+    height : 90px;
+    background-color : ;
+    position: relative;
+`;
+
+export default function table() {
+  
+  return (
+    <div>table</div>
+  )
+
+}
