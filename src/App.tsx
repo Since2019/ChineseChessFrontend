bf2424fa8f599d598459f5a4e23e89dad0e9ba05
Rickson2019 <JudaoZhong@gmail.com>
2022-07-04 23:48:19 -0700

    Added routing for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { TouchBackend } from 'react-dnd-touch-backend'
-
+import WelcomePage from "./pages/WelcomePage"
 
 import { BoardContextProvider } from './contexts/BoardContext'
 import Board from './components/Board'
@@ -14,11 +17,22 @@ function App() {
   return (
 
     <div className="App">
+
       <DndProvider backend={HTML5Backend}>
         {/* <DndProvider backend={TouchBackend}> */}
-          <BoardContextProvider>
-            <Board />
-          </BoardContextProvider>
+        <BoardContextProvider>
+
+          <BrowserRouter>
+
+            <Routes>
+
+              <Route path="/" element={<WelcomePage />} />
+              <Route path="/board/:mode" element={<Board />} />
+            </Routes>
+ 
+          </BrowserRouter >
+    
+        </BoardContextProvider>
         {/* </DndProvider> */}
       </DndProvider>
     </div>
