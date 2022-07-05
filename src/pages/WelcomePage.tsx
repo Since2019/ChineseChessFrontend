52d71dc2f272420370b01cde7206933a77b01b6f
Rickson2019 <JudaoZhong@gmail.com>
2022-07-04 23:46:11 -0700

    Added buttons for selecting playmodes

---
+++ src/pages/WelcomePage.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Table from "../components/WelcomePage/Table";
+import styled from 'styled-components';
+import { Link, useNavigate  } from "react-router-dom";
+// Styled Components
+const Button = styled.button`
+    width : 90px;
+    height : 90px;
+    background-color : ;
+    position: relative;
+`;
+
+export default function WelcomePage() {
+    // const navigate = useNavigate();
+    return (
+        <>
+            <h2>WelcomePage</h2>
+            <Table />
+            <br />
+
+            <Link to='/board/ai'>
+                <Button>
+                    Play against AI
+                </Button>
+            </Link>
+        </>
+    )
+}
